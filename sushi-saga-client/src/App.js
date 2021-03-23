4163c248d7986bf61db074d6ca1f62433eef3edd
Evan <evanahouse@gmail.com>
2021-03-23 11:45:09 -0700

    intitial sushi set up

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,10 +7,44 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  state = {
+    allSushis: [],
+    displayedSushis: [],
+    index: 0
+
+  }
+
+  componentDidMount(){
+    fetch (API)
+    .then(res => res.json())
+    .then(jsonData => this.setState({allSushis: jsonData}))
+    .then(this.handleSlice)
+    
+  }
+  
+  handleIndexing = () => {
+    let oldIndex = this.state.index
+    if (oldIndex === 100){
+      this.setState({index: 0})
+    }
+    else{
+      this.setState({index: oldIndex + 4})
+      console.log(this.state)
+    }
+    this.handleSlice()
+  }
+
+  handleSlice = () => {
+    let sushisCopy = [...this.state.allSushis]
+    let slicedSushi = sushisCopy.slice(this.state.index, this.state.index + 4)
+    this.setState({displayedSushis: slicedSushi})
+    
+  }
+
   render() {
     return (
       <div className="app">
-        <SushiContainer />
+        <SushiContainer index={this.state.index} handleIndexing={this.handleIndexing} displayedSushis={this.state.displayedSushis} displayIndex={this.state.displayIndex} />
         <Table />
       </div>
     );
